b9d0be423175b86d990a66e8148733392a05bb6b
beming-dev <mingfordev@gmail.com>
2025-07-16 22:53:20 +0900

    log

--- src/context/WebSocketContext.tsx
+++ src/context/WebSocketContext.tsx
@@ -40,7 +40,7 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
   useEffect(() => {
     const connectWebSocket = () => {
       const socketURL = import.meta.env.VITE_SOCKET_URL;
-      console.log(socketUrl)
+      console.log(socketURL)
       webSocket.current = new WebSocket(socketURL);
 
       webSocket.current.onopen = () => {
