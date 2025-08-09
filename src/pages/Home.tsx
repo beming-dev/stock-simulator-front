81f632b1344c032ea7b47c9dc7c5440e79000e64
beming-dev <mingfordev@gmail.com>
2025-08-09 20:21:27 +0900

    Update design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,20 @@ const Home: React.FC = () => {
       <p className="text-lg text-gray-700 mb-8">
         Start trading with our simulation platform.
       </p>
-      <Link
-        to="/dashboard"
-        className="bg-blue-500 text-white px-6 py-3 rounded-md shadow-md hover:bg-blue-600 transition duration-200 "
-      >
-        Go to Dashboard
-      </Link>
-      <Link
-        to="/search"
-        className="bg-blue-500 text-white px-6 py-3 rounded-md shadow-md hover:bg-blue-600 transition duration-200 mt-6"
-      >
-        Search Stock
-      </Link>
+      <div className="space-x-4">
+        <Link
+          to="/dashboard"
+          className="bg-blue-500 text-white px-6 py-3 rounded-md shadow-md hover:bg-blue-600 transition duration-200 "
+        >
+          Go to Dashboard
+        </Link>
+        <Link
+          to="/search"
+          className="bg-blue-500 text-white px-6 py-3 rounded-md shadow-md hover:bg-blue-600 transition duration-200 mt-6"
+        >
+          Search Stock
+        </Link>
+      </div>
       <div className="mt-12 w-full max-w-4xl  pt-10">
         <StockList />
       </div>
